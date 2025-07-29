8ee484d64acc233ff4cd5e3eee1675feb2abdb34
utsha <thapautsha823@gmail.com>
2025-07-29 17:37:15 +0545

    Header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import Card from "./Card.jsx";
+import Header from "./header.jsx"
 import pizzaimage from "./assets/pizza.png";
 import burgerimage from "./assets/burger.png";
 import friesimage from "./assets/fries.png";
-import chickenimage from "./assets/chicken.png";
+
 function App(){
   return(
     <>
+    <Header/>
     <Card image={pizzaimage} title="Pizza" description="Deliciously cheesy with fresh toppings!"
     />
     <Card image={burgerimage} title="Burger" description="One bite at a time,remember,lifetime!"/>
     <Card image={friesimage} title="French fries" description="Crispy on the outside, soft on the inside!"/>
-    <Card image={chickenimage} title="Chicken chilly" description="Sizzling hot, saucy, and oh-so-addictive!"/>
-    
+   
 
     </>
 
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+function Header(){
+    return(
+        <div className="head">
+            <ol>Home</ol>
+            <ol>About</ol>
+            <ol>Services</ol>
+            <ol>Contact us</ol>
+            <ol>Info</ol>
+        </div>
+    );
+}
+export default Header;
